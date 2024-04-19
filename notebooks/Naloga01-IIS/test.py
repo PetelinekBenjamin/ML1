82cbd4ad228af5297e1a814327316980a38517d9
Benjamin <benjamin.petelinek@gmail.com>
2024-04-19 22:02:25 +0200

    a

--- notebooks/Naloga01-IIS/test.py
+++ notebooks/Naloga01-IIS/test.py
@@ -4,11 +4,12 @@ import unittest
 
 class TestCodeQuality(unittest.TestCase):
     def test_code_quality(self):
-        # preverimo kakovost kode
+        # Preverimo kakovost kode
         result = subprocess.run(['flake8', '--exclude', '.git,__pycache__,venv', '--count', '--select=E9,F63,F7,F82', '--show-source', '--statistics'], stdout=subprocess.PIPE)
 
-        # Če je število najdenih napak večje od 0, test pade
+        # Preverimo, ali je test uspešen (vrne izhodno kodo 0)
         self.assertEqual(result.returncode, 1, f"Koda ni v redu. Povzetek napak: {result.stdout.decode('utf-8')}")
+        
 
 
 if __name__ == '__main__':
